d1449e6b420bafc9ab189b290f3cf3eb2ca4898f
Natalia <nikifor1114@mail.ru>
2022-06-14 09:49:00 +0700

    Feat: add Answer model

---
+++ db/migrate/20220614024401_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :body, null: false
+      t.integer :correct, default: 0
+      t.references :question, null:false, index:true, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end

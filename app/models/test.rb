b63e056ab00eabbee754fe8d20f4343ecffb7177
Natalia <nikifor1114@mail.ru>
2022-06-16 01:02:51 +0700

    Feat: add category method to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+  # Возвращает массив названий тестов в заданной категории по title
+  # => ["Основы баз данных", ...]
+  def self.category(category_title)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+    .where('categories.title = ?', category_title)
+    .order('tests.title': :desc)
+    .pluck('tests.title')
+  end
 end

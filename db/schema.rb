51127f03c5df6271e510ba18bf6d09c5850ec8bf
Natalia <nikifor1114@mail.ru>
2022-06-17 22:02:21 +0700

    Feat: add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_144019) do
+ActiveRecord::Schema.define(version: 2022_06_17_145809) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_06_15_144019) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
   end
 
   add_foreign_key "answers", "questions"

ddbf1b55df79710ebdd26a1776e06df82896cb00
Natalia <nikifor1114@mail.ru>
2022-06-21 13:59:58 +0700

    Feat: add question form h1

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  def question_header(test_title)
+    text = t('questions_page.form.h1_new', title: test_title)  if params[:action] == 'new'
+    text = t('questions_page.form.h1_edit', title: test_title) if params[:action] == 'edit'
+    text ||= ''
+  end
 end

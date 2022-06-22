696bab77e217a0f2f1ec70757c6562ba33089c13
Natalia <nikifor1114@mail.ru>
2022-06-22 23:26:59 +0700

    Fix: fix method question_header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,9 @@
 module QuestionsHelper
   def question_header(question)
     if question.new_record?
-      t('questions_page.form.h1_new', title: question.test.title)  if params[:action] == 'new'
+      t('questions_page.form.h1_new', title: question.test.title)
     else
-      t('questions_page.form.h1_edit', title: question.test.title) if params[:action] == 'edit'
+      t('questions_page.form.h1_edit', title: question.test.title)
     end
   end
 end

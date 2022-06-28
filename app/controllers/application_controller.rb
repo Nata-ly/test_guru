6d5d0bf355a832613b75e104430d6b268cd68b35
Natalia <nikifor1114@mail.ru>
2022-06-28 12:46:27 +0700

    Feat: add flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
+    flash[:notice] = t('flash.message.after_create_flash', name: resource.first_name)
     resource.admin? ? admin_tests_path : root_path
   end
 

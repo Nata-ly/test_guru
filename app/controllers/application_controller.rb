d64db508c6d2961c71908cc67449c45dee76028e
Natalia <nikifor1114@mail.ru>
2022-06-29 11:09:36 +0700

    Feat: setting locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  before_action :set_locale
+
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     flash[:notice] = t('flash.message.after_create_flash', name: resource.first_name)
     resource.admin? ? admin_tests_path : root_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end

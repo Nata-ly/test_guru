e6700bf1d985b4dcc1662719820bc41bc8f1652e
Natalia <nikifor1114@mail.ru>
2022-07-08 11:03:44 +0700

    Fix: added icon when passwords match

--- app/javascript/packs/utilites/check_password_match.js
+++ app/javascript/packs/utilites/check_password_match.js
@@ -8,11 +8,16 @@ document.addEventListener('turbolinks:load', function() {
 function check_password_match() {
   let password = document.querySelector('#user_password')
   let password_confirmation = document.querySelector('#user_password_confirmation')
-  if (password_confirmation.value && password.value != password_confirmation.value) {
-    password.style.border = '2px solid red'
-    password_confirmation.style.border = '2px solid red'
-  } else {
+  if (!password_confirmation.value) {
+    return;
+  }
+  if (password.value == password_confirmation.value) {
     password.style.border = '1px solid #ced4da'
     password_confirmation.style.border = '1px solid #ced4da'
+    this.querySelector('.octicon-check').classList.remove('hide')
+  } else {
+    password.style.border = '2px solid red'
+    password_confirmation.style.border = '2px solid red'
+    this.querySelector('.octicon-check').classList.add('hide')
   }
 }
